67a303b5017eb708bb55b93ad63739529740581c
leonid.negaschev <leonid.negaschev@gmail.com>
2022-06-14 19:04:53 +0500

    Create simple app with docker

---
+++ main.py
@@ -0,0 +1,10 @@
+import calendar
+
+print('Welcome to super calendar!\n')
+
+year = int(input('Please type year: '))
+month = int(input('Type number of month: '))
+
+print(calendar.month(year, month))
+
+print('Have a nice day!')
